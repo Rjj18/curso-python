bcf69f3f7550d26c8343cd1695e48ad5f894090a
Roger Oliveira <roger.psn12@gmail.com>
2022-09-23 14:20:10 -0200

    primeira aula de python da IFSP

---
+++ exe6.py
@@ -0,0 +1,64 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "babe8704",
+   "metadata": {},
+   "source": [
+    "6 – Modifique o programa de calculo do aumento de salário, de forma que ele calcule um aumento\n",
+    "de 15% para um salário de R$ 2500,00."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 24,
+   "id": "9b284b18",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "2875.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "salario = 2500\n",
+    "aumento = 15\n",
+    "caculo_aumento = ((2500*15)/100)+2500\n",
+    "c = 28\n",
+    "print (caculo_aumento)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e324f74f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
